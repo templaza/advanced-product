1177054d8caa4de7013507ec6434379cdf59eb9c
DuongTV <duongtv@templaza.com>
2022-04-04 17:03:17 +0700

    Updte price of single

--- templates/single/price.php
+++ templates/single/price.php
@@ -11,18 +11,18 @@ use Advanced_Product\Helper\AP_Custom_Field_Helper;
 $msrp   = get_field('ap_price_msrp', get_the_ID());
 $price  = get_field('ap_price', get_the_ID());
 
-$price_id       = AP_Custom_Field_Helper::get_id_by_post_id('ap_price', get_the_ID());
-$price_msrp_id  = AP_Custom_Field_Helper::get_id_by_post_id('ap_price_msrp', get_the_ID());
+//$price_id       = AP_Custom_Field_Helper::get_id_by_post_id('ap_price', get_the_ID());
+//$price_msrp_id  = AP_Custom_Field_Helper::get_id_by_post_id('ap_price_msrp', get_the_ID());
+//
+//$show_price         = AP_Custom_Field_Helper::get_field_display_flag('show_in_listing', $price_id);
+//$show_price_msrp    = AP_Custom_Field_Helper::get_field_display_flag('show_in_listing', $price_msrp_id);
 
-$show_price         = AP_Custom_Field_Helper::get_field_display_flag('show_in_listing', $price_id);
-$show_price_msrp    = AP_Custom_Field_Helper::get_field_display_flag('show_in_listing', $price_msrp_id);
-
-if (!empty($price) && $show_price) {
+if (!empty($price)) {
 
     $html = '<p class="uk-background-primary uk-padding-small uk-light ap-pricing">';
     $html .= sprintf('<span class="ap-price uk-h3"><b> %s</b> %s </span>',
         esc_html__(' ', AP_Functions::get_my_text_domain()), AP_Helper::format_price($price));
-    if (!empty($msrp) && $show_price_msrp) {
+    if (!empty($msrp)) {
         $html .= sprintf('<span class="ap-price-msrp"> %s  %s </span>',
             esc_html__('MSRP:', AP_Functions::get_my_text_domain()), AP_Helper::format_price($msrp));
     }
