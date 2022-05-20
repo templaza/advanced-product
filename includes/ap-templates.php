cb56b8a513acdcf707a5001aaf4416e5773faa19
trinhvanduong <duongtv@templaza.com>
2022-05-20 15:10:12 +0700

    Update override template file from templaza-framework

--- includes/ap-templates.php
+++ includes/ap-templates.php
@@ -9,8 +9,9 @@ class AP_Templates{
     protected static $cache = array();
 
     public static function locate_my_template($template_names, $load = false, $require_once = true, $args = array()){
-        $located    = '';
-        $base       = ADVANCED_PRODUCT.'/templates';
+        $located        = '';
+        $base           = ADVANCED_PRODUCT.'/templates';
+        $framework_path = ADVANCED_PRODUCT_TEMPLAZA_FRAMEWORK_TEMPLATE_PATH;
 
         foreach ( (array) $template_names as &$template_name ) {
             if ( ! $template_name ) {
@@ -25,6 +26,9 @@ class AP_Templates{
                 break;
             } elseif ( file_exists( get_template_directory() . '/' . $template_name ) ) {
                 $located = get_template_directory() . '/' . $template_name;
+                break;
+            } elseif ( file_exists( $framework_path.'/'.$template_name ) ) {
+                $located   = $framework_path.'/'.$template_name;
                 break;
             } elseif ( file_exists( ADVANCED_PRODUCT_PLUGIN_DIR_PATH.'/'.$template_name ) ) {
                 $located   = ADVANCED_PRODUCT_PLUGIN_DIR_PATH.'/'.$template_name;
