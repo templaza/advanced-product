16f62ff15cad1a58eb6f815650f797d77b0b72da
trinhvanduong <duongtv@templaza.com>
2022-08-25 16:04:25 +0700

    Update delete product in compare list

--- templates/shortcodes/advanced-product/actions/remove-product.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('ADVANCED_PRODUCT') or exit();
-?>
-<a href="" class="uk-icon-button uk-margin-small-right" data-uk-icon="icon: trash; ratio: 0.85" data-uk-tooltip="<?php
-_e('Remove this product', AP_Functions::get_my_text_domain());?>"></a>
