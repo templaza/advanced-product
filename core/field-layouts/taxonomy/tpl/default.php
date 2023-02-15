6c13e40dadfd0894f59e86b748bd568bc89d68d9
trinhvanduong <duongtv@templaza.com>
2023-02-15 12:29:01 +0700

    Update separator ',' for taxonomy field type with multiple values

--- core/field-layouts/taxonomy/tpl/default.php
+++ core/field-layouts/taxonomy/tpl/default.php
@@ -7,14 +7,16 @@ $html   = '';
 if(is_object($value)){
     $term   = $value;
 }elseif(is_array($value)){
-    foreach ($value as $val){
+    foreach ($value as $i => $val){
         if(is_numeric($val)) {
             $term   = get_term_by('term_id', $val, $field['taxonomy']);
         }else{
             $term   = get_term_by('slug', $val, $field['taxonomy']);
         }
         ?>
-        <a href="<?php echo get_term_link($term); ?>"><?php echo $term -> name; ?></a>
+        <a href="<?php echo get_term_link($term); ?>"><?php echo $term -> name; ?></a><?php
+        if($i < count($value) -1 ){ ?><span class="ap-field-taxonomy-separator"><?php echo esc_html__(',', 'advanced-product')?></span>
+        <?php }?>
         <?php
     }
 }elseif(is_numeric($value)){
