119be0f580257940a13400b3a68608299b199af5
Sonny Le <tuyennv@templaza.com>
2026-03-23 09:27:35 +0700

    fix multicategory in single inventory

--- includes/helpers/ap-custom-field-helper.php
+++ includes/helpers/ap-custom-field-helper.php
@@ -24,11 +24,16 @@ class AP_Custom_Field_Helper extends BaseHelper {
         if(!empty($ap_taxonomy_show)){
             foreach ($ap_taxonomy_show as $tax){
                 $tax_val = wp_get_object_terms($product_id,$tax);
+
                 if(! empty( $tax_val ) ){
                     if ( ! is_wp_error( $tax_val ) ) {
                         $results = $wpdb->get_row( "SELECT post_title FROM {$wpdb->prefix}posts WHERE post_excerpt = '".$tax_val[0]->taxonomy."'", ARRAY_A );
                         if($results){
-                            $tax_values[$results['post_title']] = $tax_val[0]->name;
+                            $names = [];
+                            foreach ($tax_val as $tax_item){
+                                $names[] = $tax_item->name;
+                            }
+                            $tax_values[$results['post_title']] = implode(', ', $names);
                         }
                     }
                 }
